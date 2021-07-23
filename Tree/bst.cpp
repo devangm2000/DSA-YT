20dd1845dca55eb81ac584e9a61e6653cc45eb27
Devang Mehrotra <devang.mehrotra7@gmail.com>
2021-07-23 22:07:48 +0530

    BST Insert Function

--- Tree/bst.cpp
+++ Tree/bst.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 using namespace std;
+#define SPACE 10;
 class TreeNode
 {
 public:
@@ -22,7 +23,7 @@ public:
 class BST
 {
 public:
-    TreeNode *root;
+    TreeNode *root = NULL;
     bool isEmpty()
     {
         if (root == NULL)
@@ -31,24 +32,53 @@ public:
         }
         return false;
     }
-    void InsertNode(TreeNode *new_node, int value)
+    void InsertNode(TreeNode *new_node)
     {
         if (root == NULL)
         {
-            root = new TreeNode(value);
-        }
-        else if (value > root->value)
-        {
-            // root->right=InsertNode(root->right,value);
+            root = new_node;
+            cout << "Value inserted as root node." << endl;
         }
         else
         {
+            TreeNode *temp = root;
+            while (temp != NULL)
+            {
+                if (new_node->value == temp->value)
+                {
+                    cout << "Value already exists." << endl;
+                    return;
+                }
+                else if (new_node->value < temp->value && temp->left == NULL)
+                {
+                    temp->left = new_node;
+                    cout << "Value inserted." << endl;
+                    break;
+                }
+                else if (new_node->value < temp->value)
+                {
+                    temp = temp->left;
+                    break;
+                }
+                else if (new_node->value > temp->value && temp->right == NULL)
+                {
+                    temp->right = new_node;
+                    cout << "Value inserted." << endl;
+                    break;
+                }
+                else
+                {
+                    temp = temp->right;
+                    break;
+                }
+            }
         }
     }
 };
 int main()
 {
-    int option;
+    BST obj;
+    int option, val;
     do
     {
         cout << "Enter option nmuber. Enter 0 to exit!" << endl;
@@ -58,10 +88,14 @@ int main()
         cout << "4. Print BST values" << endl;
         cout << "5. Clear Screen" << endl;
         cin >> option;
+        TreeNode *new_node = new TreeNode();
         switch (option)
         {
         case 1:
             cout << "Insert value:" << endl;
+            cin >> val;
+            new_node->value = val;
+            obj.InsertNode(new_node);
             break;
         case 2:
             cout << "Search value:" << endl;
